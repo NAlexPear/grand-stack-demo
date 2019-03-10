fe4d411d9aa96522bbdb37df4658f8a315dc088c
Alex Pearson <alex@alexpear.com>
2019-03-09 20:50:45 -0600

    :hammer: Divide GraphQL schema

--- src/api/api.js
+++ src/api/api.js
@@ -2,25 +2,31 @@ const { ApolloServer } = require('apollo-server')
 const { makeAugmentedSchema } = require('neo4j-graphql-js')
 const fs = require('fs')
 const { promisify } = require('util')
+const glob = require('glob')
 
 
 const asyncReadFile = promisify(fs.readFile)
+const asyncGlob = promisify(glob)
 
 class API {
   constructor(driver){
     this.driver = driver
   }
 
-  async fetchSchema(target) {
-    const schema = await asyncReadFile(target)
-    const typeDefs = schema.toString('utf-8')
+  async fetchSchema(schemaDirectory) {
+    const targets = await asyncGlob(`${schemaDirectory}/*.graphql`)
+    const files = targets.map(target => asyncReadFile(target))
+    const definitions = await Promise.all(files)
+    const typeDefs = definitions
+      .map(definition => definitions.toString('utf-8'))
+      .join('\n')
 
     return makeAugmentedSchema({ typeDefs })
   }
 
   async start(port = 8000) {
     const { driver } = this
-    const schema = await this.fetchSchema('./schema.graphql')
+    const schema = await this.fetchSchema('./schema')
 
     const server = new ApolloServer({
       context: { driver },
